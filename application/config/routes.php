de019299eb6514b0c3d32a4296b85fcf987d5601
jook <kittipong@imale.co.th>
2019-08-17 13:51:20 +0700

    set sun f

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,6 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$route['default_controller'] = 'ControllerLogin';
+$route['default_controller'] = 'Product';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
